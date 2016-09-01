b0751a1f171424185a2e401cb888bee0782ecfec
Kelly Kinkade <kelly.lynn.martin@gmail.com>
2016-09-01 17:49:24 -0500

    Initial commit
    Base files for an empty Minecraft (1.10.2 + Forge) mod in Scala

---
+++ src/org/pyrzqxgl/minimalScalaMod/MinimalScalaMod.scala
@@ -0,0 +1,21 @@
+/*
+ * Copyright (c) Kelly Kinkade (ab9rf) 2016
+ * https://github.com/ab9rf/minimalScalaMod
+ *
+ * This mod is distributed under the terms of the Minecraft Mod Public
+ * License 1.0, or MMPL. Please check the contents of the license located in
+ * http://....
+ */
+
+package org.pyrzqxgl.minimalScalaMod
+
+import net.minecraftforge.fml.common.Mod
+
+@Mod(modid = MinimalScalaMod.modId, version = "MINIMALSCALAMOD_VER", name = "Minimal Scala Mod", modLanguage = "scala")
+object MinimalScalaMod {
+  var instance = this
+
+  final val modId = "minimalScalaMod"
+
+}
+
